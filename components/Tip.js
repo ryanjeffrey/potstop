9ed09ce799106ef7b4ee6eac21f2badcbfe5087c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-09-22 14:53:32 -0500

    Connected tip functionality

--- components/Tip.js
+++ components/Tip.js
@@ -1,9 +1,22 @@
 import { web3 } from '../lib/web3'
 import { useState, useEffect } from 'react'
 
-const Tip = function () {
+const Tip = function ({ isLoggedIn, accounts, address }) {
   const send = function () {
-    alert("send 0.01 ETH please!")
+    if (isLoggedIn) {
+      const price = web3.utils.toWei("0.01", "ether")
+
+      window.ethereum.request({ 
+        method: "eth_sendTransaction",
+        params: [{
+          from: accounts[0],
+          to: address,
+          value: web3.utils.toHex(price)
+        }]
+      })
+    } else {
+      alert("Please connect your wallet")
+    }
   }
 
   return (
